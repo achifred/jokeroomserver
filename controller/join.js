e51d61ad689315efa449f01ab142f2f48e9f0b34
achifred <achifred123@gmail.com>
2019-10-23 00:54:36 +0000

    edited procfile

--- controller/join.js
+++ controller/join.js
@@ -3,16 +3,16 @@ const { addUser, getUserInRoom } = require('./users');
 const Onjoin = (socket, io) => {
   try {
     socket.on('join', ({ name, room }) => {
-      const { error, user } = addUser({ id: socket.id, name, room });
+      const { user } = addUser({ id: socket.id, name, room });
       //if (error) return callback(error);
 
       socket.emit('message', {
-        user: 'admin',
+        //user: 'admin',
         text: `${user.name}, welcome to the room ${user.room}`
       });
       socket.broadcast
         .to(user.room)
-        .emit('message', { user: 'admin', text: `${user.name} has joined` });
+        .emit('message', { text: `${user.name} has joined` });
       socket.join(user.room);
       io.to(user.room).emit('roomData', {
         room: user.room,
--- controller/users.js
+++ controller/users.js
@@ -2,12 +2,6 @@ const users = [];
 const addUser = ({ id, name, room }) => {
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
-  const existUser = users.find(
-    user => user.room === room && user.name === name
-  );
-  if (existUser) {
-    return { error: 'username is taken' };
-  }
 
   const user = { id, name, room };
   users.push(user);
